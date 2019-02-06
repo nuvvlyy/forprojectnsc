e049c36e7c2d711509e6c719795cf55631588970
nuvvlyy <nutnutnut_1998@hotmail.com>
2019-02-06 15:18:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/forprojectnsc/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.forprojectnsc;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://172.68.106.83/game/";
+}
